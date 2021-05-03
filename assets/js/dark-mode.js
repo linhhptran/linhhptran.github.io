1c32eb36849e8f6d6e4b6c99205816d60fad0346
tranhaiphuonglinh <l.tran161198@gmail.com>
2021-05-03 00:23:44 -0700

    find job

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,31 @@
+/* Dependency: js-cookie plugin - Ref: https://github.com/js-cookie/js-cookie */
+
+$(document).ready(function() {
+	
+	function setThemeFromCookie() {
+		// Check if the cookie is set 
+		if (typeof Cookies.get('mode') === "undefined" ) {
+			$('body').addClass("dark-mode");
+			$('#darkmode').attr('checked', true); // toggle change
+			console.log('Cookie: dark mode' );
+		} else {
+			$('body').removeClass("dark-mode");
+			$('#darkmode').attr('checked', false); // toggle change
+			console.log('Cookie: light mode' );
+		}
+	}
+	
+	setThemeFromCookie();
+	
+	$('#darkmode').on('change', function(e){
+
+		if ($(this).is(':checked')) {
+			$('body').addClass('dark-mode');
+			Cookies.remove('mode');
+		} else {
+			$('body').removeClass('dark-mode');
+			Cookies.set('mode', 'light-mode', { expires: 7 });
+		}
+	});	
+	
+});	
